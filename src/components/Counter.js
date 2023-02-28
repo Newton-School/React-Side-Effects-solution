2dfd1f03458d1a5c4ef1c474b465ee4cbe09dfd9
SanskarChandra26 <76607315+SanskarChandra26@users.noreply.github.com>
2023-02-28 14:14:51 +0530

    Done

---
+++ src/components/Counter.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+function Counter() {
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		document.title = `Count: ${count}`;
+	}, [count]);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+
+	const decrement = () => {
+		setCount(count - 1);
+	};
+
+	const reset = () => {
+		setCount(0);
+	};
+
+	return (
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={increment}>+</button>
+			<button onClick={decrement}>-</button>
+			<button onClick={reset}>Reset</button>
+		</div>
+	);
+}
+
+export default Counter;
